09c03a897fceaf4bd46db0301209f72233142b14
Beto Pimentel <adalbertopimentel.beto@gmail.com>
2026-06-11 22:37:59 -0400

    Criando a Grid, Inserindo os botões entre outras funcionalidades

---
+++ botoes.py
@@ -0,0 +1,44 @@
+from PySide6.QtWidgets import QPushButton, QGridLayout
+from variaveis import MEDIUM_FONT_SIZE
+
+
+class Button(QPushButton):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.configStyle()
+
+    def configStyle(self):
+        font = self.font()
+        font.setPixelSize(MEDIUM_FONT_SIZE)
+        self.setFont(font)
+        self.setMinimumSize(75, 75)
+
+
+class ButtonsGrid(QGridLayout):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._gridMask = [
+            ["C", "<", "^", "/"],
+            ["7", "8", "9", "*"],
+            ["4", "5", "6", "-"],
+            ["1", "2", "3", "+"],
+            ["", "0", ".", "="],
+        ]
+
+        self._makeGrid()
+
+    def _makeGrid(self):
+        for i, row in enumerate(self._gridMask):
+            for j, button_txt in enumerate(row):
+                botao = Button(button_txt)
+
+                if button_txt not in "0123456789.":
+                    botao.setProperty("cssClass", "specialButton")
+
+                self.addWidget(botao, i, j)
+
+        # for i, button_text in enumerate(buttons):
+        #     button = Button(button_text)
+        #     row = i // 4
+        #     col = i % 4
+        #     self.addWidget(button, row, col)
--- main.py
+++ main.py
@@ -5,10 +5,14 @@ from main_window import Mainwindow
 from display import Display
 from PySide6.QtGui import QIcon
 from variaveis import WINDOW_ICON_PATH
+from info import Info
+from styles import setupTheme
+from botoes import ButtonsGrid
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    setupTheme(app)
     window = Mainwindow()
 
     # Define o ícone da janela
@@ -16,9 +20,17 @@ if __name__ == "__main__":
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
 
+    # Info
+    info = Info("2.0 ^ 10.0 = 1024")
+    window.addWidgetToVLayout(info)
+
     # Display
     display = Display()
-    window.addToVLayout(display)
+    window.addWidgetToVLayout(display)
+
+    # Grid de Botões
+    grade_botoes = ButtonsGrid()
+    window.vLayout.addLayout(grade_botoes)
 
     window.show()
     app.exec()
